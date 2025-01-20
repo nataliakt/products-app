d2a5f3997773662d7385a5440b99519d89bd9d7a
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2025-01-20 14:57:50 -0300

    chore: install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*'],
+  extends: ["expo", "prettier"],
+  ignorePatterns: ["/dist/*"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
